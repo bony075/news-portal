7bf6bf8fbc142437508ea65f9f9a8d86a6842d80
bony075 <bony.075@gmail.com>
2022-10-22 17:46:51 +0600

    category menu hide and expand

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -14,7 +14,8 @@ export const routes = createBrowserRouter([
             },
             {
                 path: '/category/:id',
-                element: <Category></Category>
+                element: <Category></Category>,
+                loader: ({ params }) => fetch(`http://localhost:5000/category_news/${params.id}`)
             },
             {
                 path: '/news/:id',
